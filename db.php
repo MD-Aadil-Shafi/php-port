9444c9cb11351188ee4e366b6ac4b1e06667ad6f
Aadil <88660494+MD-Aadil-Shafi@users.noreply.github.com>
2023-02-16 11:30:51 +0530

    removed credentials

--- db.php
+++ db.php
@@ -2,10 +2,10 @@
 
 class Database{
     public $con;
-    private $servername = 'us-cdbr-east-02.cleardb.com';
-    private $username = "b43463bd16c214";
-    private $password = "4ef85be4";
-    private  $dbname = "heroku_c32efef62bf53c9";
+    private $servername = 'servername';
+    private $username = "username";
+    private $password = "password";
+    private  $dbname = "dbname";
     
     
     public function __construct(){
